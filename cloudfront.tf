9d941ba00b60a52fd14f6311df1e31b3d7c9a930
jandresleiva <andresleiva.4@gmail.com>
2025-08-24 10:16:48 -0300

    Splits CloudFront module

---
+++ cloudfront.tf
@@ -0,0 +1,63 @@
+# ---------- CloudFront (OAC) ----------
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "${aws_s3_bucket.site.bucket}-oac"
+  description                       = "OAC for ${aws_s3_bucket.site.bucket}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+  comment = "TF starter CDN"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  origin {
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    # Keep forwarded_values to preserve exact behavior/content handling.
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100"
+}
