0b22c0a1fc74482ed15e21cf35b6e2a335690cdc
jandresleiva <andresleiva.4@gmail.com>
2025-08-24 10:17:38 -0300

    Splits IAM

---
+++ iam.tf
@@ -0,0 +1,22 @@
+# ---------- Bucket policy: only this distribution can read ----------
+data "aws_iam_policy_document" "allow_cf_read" {
+  statement {
+    sid = "AllowCloudFrontServicePrincipalReadOnly"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.site.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.allow_cf_read.json
+}
--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-# ---------- Bucket policy: only this distribution can read ----------
-data "aws_iam_policy_document" "allow_cf_read" {
-  statement {
-    sid = "AllowCloudFrontServicePrincipalReadOnly"
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.site.arn}/*"]
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.cdn.arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "site" {
-  bucket = aws_s3_bucket.site.id
-  policy = data.aws_iam_policy_document.allow_cf_read.json
-}
-
 # ---------- Upload index.html from local file ----------
 resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.site.id
